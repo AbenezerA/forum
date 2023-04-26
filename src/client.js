40db2e70d10d502b06856804b591547d3e18e7ed
AbenezerA <ata2152@columbia.edu>
2023-04-25 22:19:18 -0400

    create .env and store sensitive keys there

--- src/client.js
+++ src/client.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const URL = 'https://exxkstjnlnhennuupvue.supabase.co';
-const API_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV4eGtzdGpubG5oZW5udXVwdnVlIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODA4ODczNTMsImV4cCI6MTk5NjQ2MzM1M30.UzWJt9--e9AZjvHUrIvbywM9OVoDMlAD9l9AdGaLLtA';
+const URL = import.meta.env.VITE_APP_URL;
+const API_KEY = import.meta.env.VITE_APP_API_KEY;
 
 export const supabase = createClient(URL, API_KEY);
